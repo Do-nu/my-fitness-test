4f63dcdafb82c2f84db8ac84089118b29335f553
Donubari Forghe <donuforghe@gmail.com>
2022-07-21 09:10:35 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const changeBagBg = document.getElementById("bag");
+changeBagBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeBlouseBg = document.getElementById("blouse");
+changeBlouseBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeGownBg = document.getElementById("gown");
+changeGownBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeShirtBg = document.getElementById("shirt");
+changeShirtBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeShoesBg = document.getElementById("shoes");
+changeShoesBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeShortBg = document.getElementById("short");
+changeShortBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changSkirtBg = document.getElementById("skirt");
+changSkirtBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeSneakersBg = document.getElementById("sneakers");
+changeSneakersBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
+
+const changeTrouserBg = document.getElementById("trouser");
+changeTrouserBg.addEventListener("click", (event) => {
+  event.target.style.background = "coral";
+  event.target.style.color = "white";
+});
